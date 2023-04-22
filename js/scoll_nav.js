5c87a05b48abde38d78673ada99b4139c9d6ad71
Dang Hieu <danghieutieumai@gmail.com>
2023-04-22 19:54:07 +0700

    fix js scoll not active in navbar

--- js/scoll_nav.js
+++ js/scoll_nav.js
@@ -1,6 +1,27 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".header__list li a");
+
+// mobile
+const sections1 = document.querySelectorAll("section");
+const navLi1 = document.querySelectorAll(".header-mobile__list li a");
 window.onscroll = () => {
+  if(window.scrollX < 800){
+    var current = "";
+
+    sections.forEach((section) => {
+      var pageYOffset = window.scrollY;
+      const sectionTop = section.offsetTop;
+      if (pageYOffset >= sectionTop - 60) {
+        current = section.getAttribute("id"); }
+    });
+  
+    navLi1.forEach((li) => {
+      li.classList.remove("active1");
+      if (li.classList.contains(current)) {
+        li.classList.add("active1");
+      }
+    });
+  }
   var current = "";
 
   sections.forEach((section) => {
@@ -16,25 +37,5 @@ window.onscroll = () => {
       li.classList.add("active");
     }
   });
-};
-//mobile
-const sections1 = document.querySelectorAll("section");
-const navLi1 = document.querySelectorAll(".header-mobile__list li a");
-window.onscroll = () => {
-  var current = "";
 
-  sections.forEach((section) => {
-    var pageYOffset = window.scrollY;
-    const sectionTop = section.offsetTop;
-    if (pageYOffset >= sectionTop - 60) {
-      current = section.getAttribute("id"); }
-      console.log(current);
-  });
-
-  navLi1.forEach((li) => {
-    li.classList.remove("active1");
-    if (li.classList.contains(current)) {
-      li.classList.add("active1");
-    }
-  });
 };
